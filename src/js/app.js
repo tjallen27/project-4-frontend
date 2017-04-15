82b4c50a033c701801e9f23a133a5172ae5f0af7
tjallen27 <tjallen91@hotmail.co.uk>
2017-04-15 13:05:25 +0100

    Folder set up

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,3 @@
-// app.js is the main JS file which you should define your Angular module
+angular
+  .module('Kuler', ['ui.router', 'ngResource', 'satellizer', 'checklist-model'])
+  .constant('API_URL', 'http://localhost:3000/api');
