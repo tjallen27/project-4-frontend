99c05bf08b7817c6cf13413764cc8c0a0363816a
tjallen27 <tjallen91@hotmail.co.uk>
2017-04-20 15:42:35 +0100

    Stripe form set up

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 angular
-  .module('Kuler', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate'])
-  .constant('API_URL', 'http://localhost:3000');
+  .module('Kuler', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
+  .constant('API_URL', 'http://localhost:3000')
+  .config(function() {
+  Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+});
