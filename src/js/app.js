c9879fe327e4bd7b3f37e0e91d3d3d1c5abf631d
tjallen27 <tjallen91@hotmail.co.uk>
2017-05-15 14:48:51 +0100

    API URL

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 angular
   .module('Kuler', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
-  .constant('API_URL', 'http://localhost:3000')
+  .constant('API_URL', 'https://protected-escarpment-25598.herokuapp.com/')
   .config(function() {
   Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 });
